c54999b5ac50a28043a28769d929be513478ede4
kimnagui <calrosban@gmail.com>
2020-06-29 15:17:15 +0900

    [Update] 환경변수 수정
    * 스테이지 환경변수 제대로 적용되도록 오류 수정
    * 도메인 환경변수 추가

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,4 +1,4 @@
-module.exports.CONFIG = (serverless) => ({
+module.exports.ENV = (serverless) => ({
   URL: "http://localhost:4000",
 
   TOKEN_SECRET_KEY: "YourSecretKey",
@@ -8,17 +8,27 @@ module.exports.CONFIG = (serverless) => ({
   REFRESH_TOKEN_URL: "/auth/refresh_token",
   LOGOUT_URL: "/auth/logout",
 
-  SUCCESS_REDIRECT_URL: "http://localhost:3000/auth/success",
-  FAIL_REDIRECT_URL: "http://localhost:3000/auth/fail",
-  LOGOUT_REDIRECT_URL: "http://localhost:3000/auth/logout/complete",
+  SUCCESS_REDIRECT_URL: "http://localhost:4000/auth/success",
+  FAIL_REDIRECT_URL: "http://localhost:4000/auth/fail",
+  LOGOUT_REDIRECT_URL: "http://localhost:4000/auth/logout/complete",
 
   GOOGLE_AUTH_CLIENT_ID: "your google oauth client id",
   GOOGLE_AUTH_CLIENT_SECRET: "your google oauth client secret",
   GOOGLE_AUTH_LOGIN_URL: "/auth/login/google",
   /**
-   * If stage is 'dev', full callback url is 'http://localhost:3000/dev' + GOOGLE_AUTH_LOGIN_URL + GOOGLE_AUTH_LOGIN_CALLBACK.
+   * If stage is 'dev', full callback url is 'http://localhost:4000/dev' + GOOGLE_AUTH_LOGIN_URL + GOOGLE_AUTH_LOGIN_CALLBACK.
    *
    * If stage is not 'dev', full callback url is URL + GOOGLE_AUTH_LOGIN_URL + GOOGLE_AUTH_LOGIN_CALLBACK.
    */
   GOOGLE_AUTH_LOGIN_CALLBACK: "/callback",
+});
+
+module.exports.DOMAIN = (serverless) => ({
+  domainName: "", // ex) api.nagui.me
+  hostedZoneId: "", // AWS Route-53 hosted-zone ID
+  certificateName: "", // ex) "*.nagui.me"
+  certificateArn: "", // ACM us-east-1 ARN
+  basePath: "", // ex) ""
+  stage: "", // ex) ${opt:stage, 'dev'}
+  createRoute53Record: "", // ex) true
 });
